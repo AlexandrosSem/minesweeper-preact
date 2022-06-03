6730bc69b348ae61b4608e3f38ad477e22c3b38a
Taulim <taulimtank@gmail.com>
2022-06-03 17:59:55 +0100

    Fix drawing of the board columns size

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -12,7 +12,7 @@ const mapClientToServerDiff = pDiff => {
 };
 
 export const Game = () => {
-    const [ data, setData] = useState(null);
+    const [ data, setData ] = useState(null);
     const [ diff, setDiff ] = useState('normal');
     const [ reset, setReset ] = useState(false);
     const [ blockData, setBlockData ] = useState(null);
@@ -90,6 +90,7 @@ export const Game = () => {
     return (
         <div class={style.game}>
             <GameHeader onChangeDifficulty={(pDiff) => changeDifficulty(pDiff)} status={data.status}></GameHeader>
+            <Board onClick={(pNumber) => handleSquareClick(pNumber)} blockData={blockData} numberOfRows={data.size[1]} numberOfCols={data.size[0]}></Board>
             <div id="DEBUG" style="text-align: right;">
                 <button title="DEBUG" onClick={async () => {
                     const tData = await (await fetch(`/api/debug/${data.id}`, {
