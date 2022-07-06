302379f6c330160d7cdb754802d86910a8d1f798
Alexandros Semelidis <alexandrossemelidis@gmail.com>
2022-07-06 21:51:05 +0300

    Improved WebSocketManager module

--- projects/common/src/WebSocketManager.ts
+++ projects/common/src/WebSocketManager.ts
@@ -1,39 +1,47 @@
 // Just a draft requires more work
 import { encodeData, decodeData } from 'common';
 
-const CreateWebSocket = (function() {
+const CreateWebSocket = (() => {
     const _constructorCallCheck = (pInstance, pConstructor) => {
         if (!(pInstance instanceof pConstructor)) { throw new Error("Call the constructor with 'new' keyword"); }
     };
 
+    const _setPropertiesToDefault = (pObj) => {
+        pObj.URL = '';
+        pObj.socket = null;
+        pObj.isOpen = false;
+        pObj.isClosed = false;
+        pObj.isThereAMessage = false;
+        pObj.open = null;
+        pObj.close = null;
+        pObj.messageDefault = (pEvent) => pEvent.data;
+        pObj.message = pObj.messageDefault;
+        pObj.error = (pEvent) => { console.log(pEvent); };
+    };
+
     const CreateWebSocket = function(pURL) {
         _constructorCallCheck(this, CreateWebSocket);
+        _setPropertiesToDefault(this);
         this.URL = pURL;
-        this.socket = null;
-        this.isOpen = false;
-        this.isClosed = false;
-        this.isThereAMessage = false;
-        this.open = () => {};
-        this.close = () => {};
-        this.message = (pEvent) => pEvent.data;
-        this.error = (pEvent) => { console.log(pEvent); };
     };
-    CreateWebSocket.prototype.setError = function(pFunc) { this.error = pFunc };
+    CreateWebSocket.prototype.setError = function(pFunc = (pEvent) => { console.log(pEvent); }) { this.error = pFunc; };
 
     CreateWebSocket.prototype.getData = function(pType, pData) {
         this.socket.send(encodeData({ type: pType, data: pData }));
 
         return new Promise((resolve) => {
-            const tIntervalId = setInterval(async () => {
+            const tIntervalId = setInterval(() => {
                 if (this.isThereAMessage) {
                     clearInterval(tIntervalId);
+                    const tData = decodeData(this.message());
                     this.isThereAMessage = false;
-                    resolve(decodeData(this.message()));
+                    this.message = this.messageDefault;
+                    resolve(tData);
                 }
-            }, 250);
+            }, 10);
         });
     };
-    
+
     CreateWebSocket.prototype.close = function(pFunc = (() => {})) {
         this.close = pFunc;
         this.socket.close();
@@ -42,8 +50,9 @@ const CreateWebSocket = (function() {
                 if (this.isClosed) {
                     clearInterval(tIntervalId);
                     resolve(await this.close());
+                    _setPropertiesToDefault(this);
                 }
-            }, 250);
+            }, 10);
         });
     };
 
@@ -51,19 +60,20 @@ const CreateWebSocket = (function() {
         this.open = pFunc;
         this.socket = new WebSocket(this.URL);
         this.socket.addEventListener('open', (pEvent) => {
-            this.open = this.open.bind(this, pEvent);
+            this.open = this.open.bind(null, pEvent);
             this.isOpen = true;
         });
         this.socket.addEventListener('close', (pEvent) => {
-            this.close = this.close.bind(this, pEvent);
+            this.close = this.close.bind(null, pEvent);
             this.isClosed = true;
         });
         this.socket.addEventListener('message', (pEvent) => {
-            this.message = this.message.bind(this, pEvent);
+            this.message = this.message.bind(null, pEvent);
             this.isThereAMessage = true;
         });
         this.socket.addEventListener('error', (pEvent) => {
             this.error(pEvent);
+            _setPropertiesToDefault(this);
         });
 
         return new Promise((resolve) => {
@@ -72,7 +82,7 @@ const CreateWebSocket = (function() {
                     clearInterval(tIntervalId);
                     resolve(await this.open());
                 }
-            }, 250);
+            }, 10);
         });
     };
 
